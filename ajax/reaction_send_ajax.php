201d694ded9d2ac7b6c3dc7cbc7a7d440789199f
ei1821 <ei182102807@gmail.com>
2022-05-21 15:45:29 +0900

    :bug: 文字数制限超過したコメントが追加されてしまうバグを修正した

--- ajax/reaction_send_ajax.php
+++ ajax/reaction_send_ajax.php
@@ -2,6 +2,8 @@
 
 require_once __DIR__.'/../utils/utils.php';
 
+$MAX_COMMENT_LENGTH = 128;
+
 // 処理モードの取得
 $id = 1;
 if (isset($_POST["user_id"]) == true && $_POST["user_id"] != "") {
@@ -15,7 +17,7 @@ $date = new DateTime();$date = $date->format('Y-m-d H:i:s');
 
 $res = $db->query("INSERT INTO `reactions` VALUE(null, '$room_id', '$user_id', '$is_good', '$date')");
 
-if(isset($_POST["text"]) == true && $_POST["text"] != "") {
+if(isset($_POST["text"]) == true && $_POST["text"] != "" && count($_POST["text]" < $MAX_COMMENT_LENGTH) {
   $txt = $_POST["text"];
   $res = $db->query("INSERT INTO `comments` VALUE(null, '$room_id', '$user_id', '$is_good', '$txt', '$date')");
 }
