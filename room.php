b2241232b879d644ac63e9efa75c2228b4091ce0
ei1821 <ei182102807@gmail.com>
2022-05-31 10:45:10 +0000

    :books: コメントの削除

--- room.php
+++ room.php
@@ -45,6 +45,7 @@
  <link rel="stylesheet" href="css/style.css">
  <link rel="stylesheet" href="css/comment.css">
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+ <script type="text/javascript" src="http://code.jquery.com/ui/1.10.3/jquery-ui.min.js"></script>
  <script src="https://d3js.org/d3.v7.min.js"></script>
  <script >
 var res_logs = [];
@@ -56,8 +57,6 @@ async function res_poll() {
     try {
         const response = await $.getJSON("ajax/response_ajax.php?room_id=" + id);
         res_logs.unshift(response);
-        //console.log(res_logs);
-        //$('#response').prepend("<p>good : " + response.good + " bad : " + response.bad + "</p>");
         $("#response_hiddenbutton").trigger("click");
 
     } catch (e) {
