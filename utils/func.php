2454ccf2ca30bee70f475609bc119c2f3759a527
ei1821 <ei182102807@gmail.com>
2022-06-01 13:49:22 +0000

    :recycle: 簡易メッセージを関数化

---
+++ utils/func.php
@@ -0,0 +1,9 @@
+<?php
+
+function echo_msg($str) {
+	printf("<div class='echo_msg'>%s</div>\n", $str);
+}
+
+
+?>
+
--- utils/utils.php
+++ utils/utils.php
@@ -2,5 +2,6 @@
 require_once __DIR__."/MyDB.class.php";
 require_once __DIR__."/session_start.php";
 require_once __DIR__."/define.php";
+require_once __DIR__."/func.php";
 
 ?>
