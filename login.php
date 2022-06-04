06aab08c5f17eb0e4e214b9080f508728aa780ef
ei1821 <ei182102807@gmail.com>
2022-06-04 22:29:47 +0000

    :sparkles: ログインに成功すると元のページにもどる機能を追加

--- login.php
+++ login.php
@@ -1,5 +1,33 @@
 <?php
 require_once __DIR__."/utils/utils.php";
+//フォームからの値をそれぞれ変数に代入
+$mail = $db->escape($_POST['mail']);
+
+$sql = "SELECT * FROM `users` WHERE mail = '$mail'";
+
+$ret = $db->query($sql);
+
+$member = $ret["result"][0];
+
+//指定したハッシュがパスワードにマッチしているかチェック
+if (password_verify($_POST['pass'], $member['password'])) {
+	session_regenerate_id(true);
+    //DBのユーザー情報をセッションに保存
+    $_SESSION['id'] = $member['id'];
+    $_SESSION['name'] = $member['name'];
+
+	if(isset($_SESSION["past_page"])) {
+		header("Location: " . substr($_SESSION["past_page"], 9), true, 301);
+		exit;
+	}
+
+    $msg = 'ログインしました。';
+    $link = '<a href="index.php">ホーム</a>';
+} else {
+    $msg = 'メールアドレスもしくはパスワードが間違っています。';
+    $link = '<a href="signin.php">戻る</a>';
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -15,29 +43,7 @@ require_once __DIR__."/utils/utils.php";
 <body>
 
 <?php
-//フォームからの値をそれぞれ変数に代入
-$mail = $db->escape($_POST['mail']);
-
-$sql = "SELECT * FROM `users` WHERE mail = '$mail'";
-
-$ret = $db->query($sql);
-
-$member = $ret["result"][0];
-
-
-//指定したハッシュがパスワードにマッチしているかチェック
-if (password_verify($_POST['pass'], $member['password'])) {
-	session_regenerate_id(true);
-    //DBのユーザー情報をセッションに保存
-    $_SESSION['id'] = $member['id'];
-    $_SESSION['name'] = $member['name'];
-    $msg = 'ログインしました。';
-    $link = '<a href="index.php">ホーム</a>';
-} else {
-    $msg = 'メールアドレスもしくはパスワードが間違っています。';
-    $link = '<a href="signin.php">戻る</a>';
-}
-	echo_msg("<h1>$msg</h1>\n$link");
+echo_msg("<h1>$msg</h1>\n$link");
 ?>
 
 </body>
--- utils/session_start.php
+++ utils/session_start.php
@@ -9,21 +9,41 @@
 
     $db
   */
-  require_once __DIR__."/MyDB.class.php";
-  session_start();
-  $db = new MyDB();
-  if(isset($_SESSION["id"])) {
+	require_once __DIR__."/MyDB.class.php";
+	session_start();
+	ini_set('display_errors', "On");
+	$db = new MyDB();
+	if(isset($_SESSION["id"])) {
+		$user_id = $_SESSION["id"];
+		$user_name = $_SESSION["name"];
+		$db = new MyDB();
+		if(!isset($_SESSION["room_id"])) {
+			$ret = $db->query("SELECT * FROM `rooms` WHERE owner_id = $user_id AND is_closed = 0");
+			if($ret["count"] == 1) {
+				$ret = $ret["result"][0];
+				$room_id = $ret["id"];
+				$room_name = $ret["name"];
+				$_SESSION["room_id"] = $room_id;
+				$_SESSION["room_name"] = $room_name;
+			}
+		}
+	}
 
-    $user_id = $_SESSION["id"];
-    $user_name = $_SESSION["name"];
-    $db = new MyDB();
-    $ret = $db->query("SELECT * FROM `rooms` WHERE owner_id = $user_id AND is_closed = 0");
-    if($ret["count"] == 1) {
-      $ret = $ret["result"][0];
-      $room_id = $ret["id"];
-      $room_name = $ret["name"];
-      $_SESSION["room_id"] = $room_id;
-      $_SESSION["room_name"] = $room_name;
-    }
-  }
+	$skip_pages = ["login.php", "signin.php", "signup.php", "regster.php"];
+
+	if(isset($_SESSION["current_page"])) {
+		$uri = $_SESSION["current_page"];
+		$skip_flag = false;
+		foreach($skip_pages as $str) {
+			if(preg_match("/" . $str . "/", $uri)) {
+				$skip_flag = true;
+				break;
+			}
+		}
+		if(!$skip_flag) {
+			$_SESSION["past_page"] = $_SESSION["current_page"];
+		}
+	}
+	$_SESSION["current_page"] = $_SERVER["REQUEST_URI"];
+
 ?>
